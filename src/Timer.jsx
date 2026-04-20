d8ad62309582d201849f58707d11bec4e2c7e6de
Mohsin Ansari <mohxinmuhammad>
2026-04-20 11:48:42 +0500

    fix

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -5,22 +5,23 @@ function Timer({startTime}) {
   const [time, setTime] = useState(0)
   const intervalRef = useRef(null)
 
-  const formattedTime = startTime
-    ? new Date(startTime + time).toLocaleTimeString(undefined, {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        timeZoneName: 'short',
-      })
-    : '00:00:00'
+  // const formattedTime = startTime
+  //   ? new Date(startTime + time).toLocaleTimeString(undefined, {
+  //       hour: '2-digit',
+  //       minute: '2-digit',
+  //       second: '2-digit',
+  //       timeZoneName: 'short',
+  //     })
+  //   : '00:00:00'
   useEffect(() => {
     if (startTime) {
+      // on mount component
       intervalRef.current = setInterval(() => {
-        console.log(Date.now() - startTime);
+        console.log(Date.now() - startTime, 'to check timer is running or not');
         setTime(Date.now() - startTime)
       }, 1000)
       
-      // on unmount component
+      // on unmount component (Commentout to check timer is running or not)
       return () => {
         if (intervalRef.current) clearInterval(intervalRef.current)
         intervalRef.current = null
@@ -34,7 +35,7 @@ function Timer({startTime}) {
 
   return (
     <div>
-      {formattedTime}
+      {time}
     </div>
   )
 }
